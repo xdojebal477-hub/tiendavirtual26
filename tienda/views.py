f555fad367014ea0b24b90f2652c0ae52a1ea934
danojeda205 <xdojebal477@ieshnosmachado.org>
2026-01-21 14:07:17 +0100

    seguir con el ejercicio

--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView,DeleteView
 
 # Create your views here.
+class ListadoProductos(ListView):
+    model=Producto
+    template_name='tienda/productos_lista.html'
+    context_object_name='productos'
